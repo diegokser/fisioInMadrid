cdb6a9b206de58a98cc3486156388ddbd0909103
Diego Casermeiro Gálvez <122561493+diegokser@users.noreply.github.com>
2023-10-25 20:23:50 +0000

    creación fichero metodo

--- src/front/js/pages/tarifas.js
+++ src/front/js/pages/tarifas.js
@@ -67,25 +67,25 @@ export const Tarifas = () =>{
                                  </p>
                             </div>
                             <div className="col-12 col-xl-6 all-carousel-tarifas">
-                                <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-                                <div class="carousel-inner">
-                                    <div class="carousel-item active">
-                                        <img src={imagenPalo} class="d-block img-carousel-tarifas" alt="..."/>
+                                <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active">
+                                        <img src={imagenPalo} className="d-block img-carousel-tarifas" alt="..."/>
                                     </div>
-                                    <div class="carousel-item">
-                                        <img src={imagenPesas} class="d-block img-carousel-tarifas" alt="..."/>
+                                    <div className="carousel-item">
+                                        <img src={imagenPesas} className="d-block img-carousel-tarifas" alt="..."/>
                                     </div>
-                                    <div class="carousel-item">
-                                        <img src={imagenEscritura} class="d-block img-carousel-tarifas" alt="..."/>
+                                    <div className="carousel-item">
+                                        <img src={imagenEscritura} className="d-block img-carousel-tarifas" alt="..."/>
                                     </div>
                                 </div>
-                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                    <span class="visually-hidden">Previous</span>
+                                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span className="visually-hidden">Previous</span>
                                 </button>
-                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                    <span class="visually-hidden">Next</span>
+                                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span className="visually-hidden">Next</span>
                                 </button>
                                 </div>
                             </div>
